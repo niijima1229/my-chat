a6e2384739ee888666caeda71155d1afa5aa2ccb
新島　駿 <niijima131229@docomo.ne.jp>
2022-09-23 19:45:56 +0900

    ユーザーの追加処理、削除処理、更新処理を追加

--- api/controller/user.go
+++ api/controller/user.go
@@ -7,8 +7,27 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func UserController(c *gin.Context) {
+func GetUsers(c *gin.Context) {
 	users := []models.User{}
 	database.DB.Find(&users)
-	c.String(200, "Hello World!!")
+	c.JSON(200, &users)
+}
+
+func CreateUsers(c *gin.Context) {
+	var user models.User
+	c.BindJSON(&user)
+	database.DB.Create(&user)
+	c.JSON(200, &user)
+}
+
+func DeleteUser(c *gin.Context) {
+	var user models.User
+	database.DB.Where("id = ?", c.Param("id")).Delete(&user)
+	c.JSON(200, &user)
+}
+
+func UpdateUser(c *gin.Context) {
+	users := []models.User{}
+	database.DB.Find(&users)
+	c.JSON(200, &users)
 }
