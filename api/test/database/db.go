8a975ed59088f971fb60dcaf5278b72e6b14863e
新島　駿 <niijima131229@docomo.ne.jp>
2022-09-24 00:24:28 +0900

    テストデータベースとの接続部とユーザーテーブルの内容修正

---
+++ api/test/database/db.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"fmt"
+	"my-joke-book/database/models"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func TestConnectDB() {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tokyo", os.Getenv("TEST_DB_HOST"), os.Getenv("TEST_DB_USER"), os.Getenv("TEST_DB_PASSWORD"), os.Getenv("TEST_DB_NAME"), os.Getenv("TEST_DB_PORT"))
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("DB error(Init): ", err)
+	}
+	db.AutoMigrate(&models.User{})
+	DB = db
+}
