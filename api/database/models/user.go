a7e56426466958f4ebd89a660d3ab91314211fae
新島　駿 <niijima131229@docomo.ne.jp>
2022-09-24 00:37:40 +0900

    Userモデルの定義を変更

--- api/database/models/user.go
+++ api/database/models/user.go
@@ -4,8 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Id       uint   `json:"id" gorm:primary_key`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Name     string
+	Email    string
+	Password string
 }
