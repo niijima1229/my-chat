257f0a34d463494724cdf53441d4825804e50695
新島　駿 <niijima131229@docomo.ne.jp>
2022-09-23 22:14:26 +0900

    テスト用のDBコンテナの設定と、環境変数の設定、alpineOSの使用をやめた、goのバージョンを1.19に変更

--- api/database/db.go
+++ api/database/db.go
@@ -3,6 +3,7 @@ package database
 import (
 	"fmt"
 	"my-joke-book/database/models"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -11,7 +12,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := "host=db user=postgres password=postgres dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Tokyo"
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tokyo", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println("DB error(Init): ", err)
