c8cb58ab5adc325f7e4f4763cd3c1e94a14f1a22
新島　駿 <niijima131229@docomo.ne.jp>
2022-09-24 16:27:10 +0900

    パスワードをハッシュ化して保存

--- api/database/db.go
+++ api/database/db.go
@@ -5,11 +5,17 @@ import (
 	"my-joke-book/database/models"
 	"os"
 
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
 
 func ConnectDB() {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tokyo", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
@@ -18,10 +24,14 @@ func ConnectDB() {
 		fmt.Println("DB error(Init): ", err)
 	}
 	db.AutoMigrate(&models.User{})
+	hash, err := HashPassword("password")
+	if err != nil {
+		fmt.Println("Hash error(Init): ", err)
+	}
 	var users = []models.User{
-		{Name: "jinzhu1", Email: "test@com", Password: "password"},
-		{Name: "jinzhu2", Email: "test2@com", Password: "password"},
-		{Name: "jinzhu3", Email: "test3@com", Password: "password"}}
+		{Name: "jinzhu1", Email: "test@com", Password: hash},
+		{Name: "jinzhu2", Email: "test2@com", Password: hash},
+		{Name: "jinzhu3", Email: "test3@com", Password: hash}}
 	db.Create(&users)
 	DB = db
 }
--- api/go.mod
+++ api/go.mod
@@ -27,7 +27,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
-	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/crypto v0.0.0-20220924013350-4ba4fb4dd9e7 // indirect
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
 	golang.org/x/text v0.3.7 // indirect
