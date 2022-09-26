b24d0ce5b4b3f461b2c65a3f1ad21f74f6e433cb
新島　駿 <niijima131229@docomo.ne.jp>
2022-09-26 11:59:36 +0900

    connect db関数をdbポインタを返す形に変更

--- api/controller/user.go
+++ api/controller/user.go
@@ -8,26 +8,30 @@ import (
 )
 
 func GetUsers(c *gin.Context) {
+	db := database.ConnectDB()
 	users := []models.User{}
-	database.DB.Find(&users)
+	db.Find(&users)
 	c.JSON(200, &users)
 }
 
 func CreateUsers(c *gin.Context) {
 	var user models.User
+	db := database.ConnectDB()
 	c.BindJSON(&user)
-	database.DB.Create(&user)
+	db.Create(&user)
 	c.JSON(200, &user)
 }
 
 func DeleteUser(c *gin.Context) {
 	var user models.User
-	database.DB.Where("id = ?", c.Param("id")).Delete(&user)
+	db := database.ConnectDB()
+	db.Where("id = ?", c.Param("id")).Delete(&user)
 	c.JSON(200, &user)
 }
 
 func UpdateUser(c *gin.Context) {
 	users := []models.User{}
-	database.DB.Find(&users)
+	db := database.ConnectDB()
+	db.Find(&users)
 	c.JSON(200, &users)
 }
--- api/database/db.go
+++ api/database/db.go
@@ -10,19 +10,17 @@ import (
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
-
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
 }
 
-func ConnectDB() {
+func ConnectDB() *gorm.DB {
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Tokyo", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println("DB error(Init): ", err)
 	}
 	db.AutoMigrate(&models.User{}, &models.Joke{}, &models.Genre{})
-	DB = db
+	return db
 }
