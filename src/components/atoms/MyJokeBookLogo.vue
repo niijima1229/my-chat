761063846061145e97b107ae7908ea9f21ecf4c4
新島　駿 <niijima131229@docomo.ne.jp>
2022-09-27 21:14:29 +0900

    ヘッダー作成

---
+++ src/components/atoms/MyJokeBookLogo.vue
@@ -0,0 +1,10 @@
+<template>
+  <img class="main-logo" src="/logo_transparent.webp" alt="main logo" />
+</template>
+
+<style>
+.main-logo {
+  height: 50px;
+  width: 50px;
+}
+</style>
---
+++ src/components/molecules/MyJokeBookLogo.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-row class="align-center">
+    <AtomsMyJokeBookLogo />
+    <p class="mb-0 text-md-h4 text-lg-h3 grey--text text--darken-4">
+      My Joke Book
+    </p>
+  </v-row>
+</template>
